88380a444455a5f572354348e39bc2894d70297f
Oury93 <wourymaths93@gmail.com>
2019-03-23 12:57:53 +0000

    [PUSH] first commit

---
+++ src/main/java/com/esp/dakar/WsTestApplication.java
@@ -0,0 +1,12 @@
+package com.esp.dakar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsTestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/esp/dakar/WsTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.esp.dakar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WsTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
